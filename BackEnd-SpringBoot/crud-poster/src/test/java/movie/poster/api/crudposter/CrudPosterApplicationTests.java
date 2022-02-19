3626f9676cb03185f87f5f997a29006e9a32784d
julian.corrales <julian.corrales@interedes.com.co>
2022-02-19 18:59:53 -0500

    Springboot init

---
+++ BackEnd-SpringBoot/crud-poster/src/test/java/movie/poster/api/crudposter/CrudPosterApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.poster.api.crudposter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
